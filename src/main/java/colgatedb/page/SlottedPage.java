65152c41eebe44dadf3d2cbeb72ce2e2ccaac755
cemezachary <zceme@colgate.edu>
2018-09-11 16:35:12 -0400

    changes

--- src/main/java/colgatedb/page/SlottedPage.java
+++ src/main/java/colgatedb/page/SlottedPage.java
@@ -80,6 +80,10 @@ public class SlottedPage implements Page {
      */
     public boolean isSlotUsed(int slotno) {
         throw new UnsupportedOperationException("implement me!");
+        /*if (slotno != 0){
+            return true;
+        }
+        return false;*/
     }
 
     /**
@@ -104,6 +108,14 @@ public class SlottedPage implements Page {
      */
     public int getNumEmptySlots() {
         throw new UnsupportedOperationException("implement me!");
+        //the iterator will go through each element that is a tuple.
+        //It'll skip all of the null elements. Can I possibly keep track
+        //of the amount that is in the list then subtract that from the
+        //total to get the # of empty slots
+        
+        /* Or maybe just make a forloop and whenever isSlotEmpty() == true,
+        add 1 to the count
+         */
     }
 
     /**
@@ -112,6 +124,7 @@ public class SlottedPage implements Page {
      * @throws PageException if slot is empty
      */
     public Tuple getTuple(int slotno) {
+        if (isSlotEmpty(slotno)){ throw new PageException();}
         throw new UnsupportedOperationException("implement me!");
     }
 
